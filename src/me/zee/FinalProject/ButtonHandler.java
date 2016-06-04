0b6ef5f855293cac0d2cc0e63790e76fd795d46f
Zee <amurphey7@gmail.com>
2016-06-04 14:22:42 -0400

    Moved around variable names to match up with new order of buttons in main menu

--- src/me/zee/FinalProject/ButtonHandler.java
+++ src/me/zee/FinalProject/ButtonHandler.java
@@ -7,13 +7,13 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 public class ButtonHandler implements ActionListener {
-	private JButton start, quit, help;
+	private JButton start, help, quit;
 	private String title;
 	private double wide, tall;
-	public ButtonHandler(JButton start, JButton quit, JButton help, String title, double wide, double tall) {
+	public ButtonHandler(JButton start, JButton help, JButton quit, String title, double wide, double tall) {
 		this.start = start;
-		this.quit = quit;
 		this.help = help;
+		this.quit = quit;
 		this.title = title;
 		this.wide = wide;
 		this.tall = tall;
@@ -26,13 +26,13 @@ public class ButtonHandler implements ActionListener {
 			new Game(title, wide, tall);
 		}
 		
-		else if (e.getSource()==quit) {
-			int dialogButton = JOptionPane.showConfirmDialog(null, "Are you sure you want to end the program?","Confirmation", JOptionPane.YES_NO_OPTION);
-			if (dialogButton == JOptionPane.YES_OPTION) System.exit(0);
+		else if (e.getSource()==help) { //Didn't feel like setting up a help menu/window yet, so we'll have this for now
+			JOptionPane.showMessageDialog(null, "Help? Help is for women. We are men!", "Help", JOptionPane.INFORMATION_MESSAGE);
 		}
 		
-		else if (e.getSource()==help) {
-			JOptionPane.showMessageDialog(null, "Help? Help is for women. We are men!", "Help", JOptionPane.INFORMATION_MESSAGE);
+		else if (e.getSource()==quit) {
+			int dialogButton = JOptionPane.showConfirmDialog(null, "Are you sure you want to end the program?", "Confirmation", JOptionPane.YES_NO_OPTION);
+			if (dialogButton == JOptionPane.YES_OPTION) System.exit(0);
 		}
 	}
 }
