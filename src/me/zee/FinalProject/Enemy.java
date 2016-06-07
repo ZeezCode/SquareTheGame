47fcd7de211cc4380d0c2481007b0c838f7e3ab2
Zee <amurphey7@gmail.com>
2016-06-06 21:26:13 -0400

    Cleaned up code and added JavaDoc stuff to methods, likely gonna do the same for other classes soon

--- src/me/zee/FinalProject/Enemy.java
+++ src/me/zee/FinalProject/Enemy.java
@@ -11,6 +11,13 @@ public class Enemy {
 	private CollidableImageElement imageElement;
 	private Game game;
 	
+	/**
+	 * <p>Regular constructor</p>
+	 * <p>NOTE: Creating this enemy does not add it to the Window. That is not handled by this class. (It may in the future though?)</p>
+	 * 
+	 * @param imgElement The initial CollidableImageElement that represents this enemy
+	 * @param isAlive Whether or not the enemy should be considered alive
+	 */
 	public Enemy(CollidableImageElement imgElement, Game game) {
 		this.UniqueID = UUID.randomUUID();
 		this.isAlive = true;
@@ -18,42 +25,87 @@ public class Enemy {
 		this.game = game;
 	}
 	
+	/**
+	 * <p>Regular constructor, don't know if I'll ever use this but who knows</p>
+	 * <p>NOTE: Creating this enemy does not add it to the Window. That is not handled by this class. (It may in the future though?)</p>
+	 * 
+	 * @param imgElement The initial CollidableImageElement that represents this enemy
+	 * @param isAlive Whether or not the enemy should be considered alive
+	 */
 	public Enemy(CollidableImageElement imgElement, boolean isAlive) {
 		this.UniqueID = UUID.randomUUID();
 		this.isAlive = isAlive;
 		this.imageElement = imgElement;
 	}
 	
+	/**
+	 * <p>Returns the UUID of this enemy</p>
+	 * <p>This may end up being removed, doubt I'll be needing it.</p>
+	 * 
+	 * @return UUID The UUID of this enemy
+	 */
 	public UUID getUniqueID() {
 		return this.UniqueID;
 	}
+	/**
+	 * <p>Sets the UUID of the enemy</p>
+	 * <p>This may end up being removed, doubt I'll be needing it.</p>
+	 * 
+	 * @param uuid The new UUID of the enemy
+	 */
 	public void setUniqueID(UUID uuid) {
 		this.UniqueID = uuid;
 	}
 	
+	/**
+	 * <p>Returns whether or not this enemy is currently alive</p>
+	 * @return boolean Whether or not the enemy is currently alive
+	 */
 	public boolean isAlive() {
 		return this.isAlive;
 	}
-	public void setIsAlive(boolean isAlive) {
-		this.isAlive = isAlive;
-	}
 	
+	/**
+	 * <p>Returns the CollidableImageElement that this enemy is currently represented by</p>
+	 * 
+	 * @return CollidableImageElement The CollidableImageElement that the enemy is currently represented by
+	 */
 	public CollidableImageElement getImageElement() {
 		return this.imageElement;
 	}
+	
+	/**
+	 * <p>Sets the CollidableImageElement that the enemy will be visually represented by</p>
+	 * 
+	 * @param imgElement The CollidableImageElement that the enemy will be visually represented by
+	 */
 	public void setImageElement(CollidableImageElement imgElement) {
 		this.imageElement = imgElement;
 	}
 	
+	/**
+	 * <p>Returns the direction that the enemy is moving in</p>
+	 * 
+	 * @return MOVE_DIRECTION The direction that the player is moving in
+	 */
 	public MOVE_DIRECTION getMoveDirection() {
 		return this.moveDirection;
 	}
+	
+	/**
+	 * <p>Sets the direction that the enemy is moving in</p>
+	 * 
+	 * @param moveD The direction the enemy is being set to move in
+	 */
 	public void setMoveDirection(MOVE_DIRECTION moveD) {
-			this.moveDirection = moveD;
+		this.moveDirection = moveD;
 	}
 	
+	/**
+	 * <p>Sets the isAlive status to false and removes image from game screen</p>
+	 */
 	public void kill() {
-		setIsAlive(false);
+		this.isAlive = false;
 		game.gameScreen.removeElement(imageElement);
 	}
 }
